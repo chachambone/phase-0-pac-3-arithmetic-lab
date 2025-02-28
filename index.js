0d5d0a31b02dbd786859a330079e1faff6f59624
Marycharity <marycharity5@gmail.com>
2025-02-28 17:15:23 +0300

    Lab Complete

---
+++ index.js
@@ -0,0 +1,31 @@
+function add(a, b) {
+    return a + b;
+}
+
+function subtract(a, b) {
+    return a - b;
+}
+
+function multiply(a, b) {
+    return a * b;
+}
+
+function divide(a, b) {
+    return a / b;
+}
+
+function increment(a) {
+    return a + 1;
+}
+
+function decrement(a) {
+    return a - 1;
+}
+
+function makeInt(str) {
+    return parseInt(str,10);
+} 
+
+function preserveDecimal(str) {
+    return parseFloat(str);
+}
